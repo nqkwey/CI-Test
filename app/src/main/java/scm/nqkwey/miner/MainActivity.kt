aecbac00a26d356653b7d0233eddd64f32793672
enagibin <enagibin@gmail.com>
2017-12-22 17:00:04 +0600

    initial commit

---
+++ app/src/main/java/scm/nqkwey/miner/MainActivity.kt
@@ -0,0 +1,13 @@
+package scm.nqkwey.miner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import scm.nqkwey.ci_miner.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
